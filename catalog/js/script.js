6b8a9482ad635757c9c3f62a13fba9b8c80e3f17
ilf <ilf.mav@gmail.com>
2020-12-19 01:27:26 +0300

    js fix

--- catalog/js/script.js
+++ catalog/js/script.js
@@ -1,6 +1,6 @@
-const more = document.querySelector('.filter__more');
-const description = document.querySelector('.filter__description')
+const more = document.querySelector('.description__more');
+const description = document.querySelector('.description__description')
 more.addEventListener('click', ()=> {
-    more.classList.add('filter__more--hide');
-    description.classList.add('filter__description--show');
+    more.classList.add('description__more--hide');
+    description.classList.add('description__description--show');
 })
